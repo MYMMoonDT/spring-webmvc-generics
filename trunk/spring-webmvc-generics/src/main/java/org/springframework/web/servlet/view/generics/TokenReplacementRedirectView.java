7688d317ae2901ded1f019f5d483db4a3e4747b2
brian.dilley@gmail.com <brian.dilley@gmail.com@ac97070a-c854-0410-b750-95c132d23843>
2008-08-20 19:21:44 +0000

    created TokenReplacementRedirectView and TokenReplacementRedirectViewResolver

---
+++ trunk/spring-webmvc-generics/src/main/java/org/springframework/web/servlet/view/generics/TokenReplacementRedirectView.java
@@ -0,0 +1,78 @@
+package org.springframework.web.servlet.view.generics;
+
+import java.net.URLEncoder;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.view.AbstractUrlBasedView;
+
+/**
+ * A view that does token replacement on a url
+ * and performs a redirect to said url. This view
+ * is useful in conjuction with controllers that
+ * want to perform POST-REDIRECT-GET functionality.
+ */
+public class TokenReplacementRedirectView 
+    extends AbstractUrlBasedView {
+
+    private int httpStatusCode = HttpServletResponse.SC_SEE_OTHER;
+    private boolean contextRelative = true;
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    protected void renderMergedOutputModel(
+        Map model, HttpServletRequest request, HttpServletResponse response)
+        throws Exception {
+        
+        // build the url
+        StringBuilder buff = new StringBuilder();
+        
+        // append context path
+        if (getUrl().startsWith("/") && this.contextRelative) {
+            buff.append(request.getContextPath());
+        }
+        buff.append(getUrl());
+        
+        // the url we're redirecting to
+        String url = buff.toString();
+        
+        // loop through and do token replacement
+        for (Object key : model.keySet()) {
+            String name = "\\{"+key.toString()+"\\}";
+            String val = model.get(key).toString();
+            url = url.replaceAll(name, URLEncoder.encode(val, "UTF-8"));
+        }
+        
+        // send the redirect
+        response.setStatus(httpStatusCode);
+        response.setHeader("Location", url);
+    }
+
+    /**
+     * Sets the HTTP status code to use for the redirect.  The
+     * default is {@link HttpServletResponse#SC_SEE_OTHER}
+     * (300).  One should be careful when setting this because
+     * not all HTTP status codes are for redirecting.
+     * @param httpStatusCode the httpStatusCode to set
+     */
+    public void setHttpStatusCode(int httpStatusCode) {
+        this.httpStatusCode = httpStatusCode;
+    }
+
+    /**
+     * Sets whether redirects are context relative or
+     * not.  By default this is set to true and only
+     * urls that start with a forward slash ("/") are
+     * redirected in a context relative manner.
+     * @param contextRelative the contextRelative to set
+     */
+    public void setContextRelative(boolean contextRelative) {
+        this.contextRelative = contextRelative;
+    }
+    
+}
