8aaf3acedb4264c31e57587b41550813c6622d8b
brian.dilley@gmail.com <brian.dilley@gmail.com@ac97070a-c854-0410-b750-95c132d23843>
2008-09-04 19:57:59 +0000

    added AbstractGenericValidator

---
+++ trunk/spring-webmvc-generics/src/main/java/org/springframework/validation/AbstractGenericValidator.java
@@ -0,0 +1,58 @@
+package org.springframework.validation;
+
+import org.springframework.web.servlet.generics.util.GenericsUtil;
+
+/**
+ * <p>
+ *  Generic implementation of spring's
+ *  {@link org.springframework.validation.Validator}.
+ * </p>
+ *
+ * <p>
+ *  Features added to this Validator not present in the one
+ *  provided by the spring framework are:
+ *  <ul>
+ *      <li>The supported class is automatically determined</li>
+ *  </ul>
+ * </p>
+ * 
+ * @param <T>
+ */
+public abstract class AbstractGenericValidator<T> 
+    implements Validator {
+
+    private Class<?> clazz =  null;
+    
+    /**
+     * Creates the AbstractGenericValidator.
+     */
+    public AbstractGenericValidator() {
+        clazz = GenericsUtil.getTypeVariableClassByName(
+            this.getClass(), AbstractGenericValidator.class, "T", true);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("unchecked")
+    public final boolean supports(Class clazz) {
+        return clazz.isAssignableFrom(this.clazz);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public final void validate(Object target, Errors errors) {
+        doValidate((T)target, errors);
+    }
+    
+    /**
+     * Generic version of {@link Validator#validate(Object, Errors)}.
+     * @param target the object to validate
+     * @param errors the errors
+     */
+    protected abstract void doValidate(T target, Errors errors);
+
+}
